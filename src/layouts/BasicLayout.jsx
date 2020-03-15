80b442b0f0013b7d70679346834d66bc64edd758
ferry <ferry_sy@163.com>
2020-03-15 12:56:13 +0800

    将首页图标替换

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -13,7 +13,7 @@ import { Result, Button } from 'antd';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
 import { isAntDesignPro, getAuthorityFromRouter } from '@/utils/utils';
-import logo from '../assets/logo.svg';
+import logo from '../assets/ferry.jpg';
 const noMatch = (
   <Result
     status={403}
