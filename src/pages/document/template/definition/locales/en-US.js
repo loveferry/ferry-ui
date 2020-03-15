bf24c85030615d022f48ddeb5f018d72e11c3e07
ferry <ferry_sy@163.com>
2020-03-15 11:00:37 +0800

    国际化语言只保留简体中文和英文

--- src/pages/document/template/definition/locales/en-US.js
+++ src/pages/document/template/definition/locales/en-US.js
@@ -14,33 +14,4 @@ export default {
   'documentTemplateDefinition.image.tooltip': 'Definition image for Template Manage page',
   'documentTemplateDefinition.form.submit': 'Submit',
   'documentTemplateDefinition.form.optional': ' (optional) ',
-
-
-
-
-  'documentTemplateDefinition.date.required': 'Please select the start and end date',
-
-  'documentTemplateDefinition.standard.required': 'Please enter a metric',
-  'documentTemplateDefinition.form.get-captcha': 'Get Captcha',
-  'documentTemplateDefinition.captcha.second': 'sec',
-  'documentTemplateDefinition.form.save': 'Save',
-  'documentTemplateDefinition.email.placeholder': 'Email',
-  'documentTemplateDefinition.password.placeholder': 'Password',
-  'documentTemplateDefinition.confirm-password.placeholder': 'Confirm password',
-  'documentTemplateDefinition.phone-number.placeholder': 'Phone number',
-  'documentTemplateDefinition.verification-code.placeholder': 'Verification code',
-
-
-  'documentTemplateDefinition.placeholder.start': 'Start date',
-  'documentTemplateDefinition.placeholder.end': 'End date',
-  'documentTemplateDefinition.standard.placeholder': 'Please enter a metric',
-  'documentTemplateDefinition.public.label': 'Target disclosure',
-  'documentTemplateDefinition.label.help': 'Customers and invitees are shared by default',
-  'documentTemplateDefinition.radio.public': 'Public',
-  'documentTemplateDefinition.radio.partially-public': 'Partially public',
-  'documentTemplateDefinition.radio.private': 'Private',
-  'documentTemplateDefinition.publicUsers.placeholder': 'Open to',
-  'documentTemplateDefinition.option.A': 'Colleague A',
-  'documentTemplateDefinition.option.B': 'Colleague B',
-  'documentTemplateDefinition.option.C': 'Colleague C',
 };
